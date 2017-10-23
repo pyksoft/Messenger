b492853838dd142f9945f38eb8768a99cac17b54
pyksoft <pyksoft@gmail.com>
2017-10-23 12:06:31 +1100

    Create models: user, conversation, messages

---
+++ db/migrate/20171022233626_create_messages.rb
@@ -0,0 +1,15 @@
+class CreateMessages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :messages do |t|
+      t.references :conversation, foreign_key: true
+      # t.references :sender, foreign_key: true
+      t.references :sender, foreign_key: { to_table: :users }
+      t.text :content
+
+      t.timestamps
+
+      # Add index to efficiently allow chronological sorting
+      t.index :created_at
+    end
+  end
+end
