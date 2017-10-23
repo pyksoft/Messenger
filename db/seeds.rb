044f03695a166041285286da4350bc1b6a254bf5
pyksoft <pyksoft@gmail.com>
2017-10-23 12:06:40 +1100

    Create seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create! [
+  { email: 'one@example.com', password: 'password' },
+  { email: 'two@example.com', password: 'password' },
+  { email: 'three@example.com', password: 'password' },
+  { email: 'four@example.com', password: 'password' },
+  { email: 'five@example.com', password: 'password' }
+]
