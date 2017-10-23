cc93c33a37912f7d9c9c604e17ba79d3d679565b
pyksoft <pyksoft@gmail.com>
2017-10-23 18:54:40 +1100

    Add scss styling for conversations and messages

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,11 +1,5 @@
 module MessagesHelper
   def parse_emoji(message)
-    # message.gsub ":laughing:", "😂"
-    # message.gsub ":smile:", "😊"
-    # message.gsub ":worried:", "😕"
-    # message.gsub ":kissing_heart:", "😘"
-    # message.gsub ":grin:", "😬"
-
     h(message).to_str.gsub(/:([\w+-]+):/) do |match|
       if emoji = Emoji.find_by_alias($1)
         %(<img alt="#$1" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
